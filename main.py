dfef8b3e25a40a70a0a8de9a52fe2887f488dc18
Sabina <sabinca98@list.ru>
2021-03-22 09:38:11 +0300

    try

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
-c =open('test_schedule.inc')
-print(c)
+from lib import schedule_parser
+c = open('test_schedule.inc')
+text = c.re ad()
+print(text)
+
+if __name__ == "__main__":
+    keywords = ("DATES", "COMPDAT", "COMPDATL")
+    parameters=("")
+    input_file = "input_data/test_schedule.inc"
+    output_csv = ""
+
+    schedule_df = schedule_parser.transform(keywords,parameters,input_file,output_csv)
+Hello world
+
+try again
